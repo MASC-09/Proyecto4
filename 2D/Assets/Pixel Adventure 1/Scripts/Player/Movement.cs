a83624f766755378ad3d0c8de6e640e11cace7db
Miguel Soto <miguelsoto@Miguels-MacBook-Pro.local>
2023-01-25 20:20:48 -0600

    animation, enemigos

--- 2D/Assets/Pixel Adventure 1/Scripts/Player/Movement.cs
+++ 2D/Assets/Pixel Adventure 1/Scripts/Player/Movement.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float runSpeed = 2; 
+    public float walkSpeed = 2;
+    public float runSpeed = 3; 
     public float jumpSpeed = 3;
     Rigidbody2D rb; //this is the player. Gets assigned on the Unity
     SpriteRenderer sr;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         rb = GetComponent<Rigidbody2D>();    
         sr = GetComponent<SpriteRenderer>();
     }
@@ -19,24 +22,48 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKey("a"))
+        
+
+        if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            rb.velocity = new Vector2(-runSpeed, rb.velocity.y);
-            sr.flipX = true;
-        } 
-        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKey("d"))
-        {
-            rb.velocity = new Vector2(runSpeed, rb.velocity.y);
+            rb.velocity = new Vector2(walkSpeed, rb.velocity.y);
             sr.flipX = false;
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb.velocity = new Vector2(-walkSpeed, rb.velocity.y);
+            sr.flipX = true;
         }
         else
         {
-          rb.velocity = new Vector2(0, rb.velocity.y);  
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
-
         if (Input.GetKey("space") && CheckGround.isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed); 
         }
+        if (CheckGround.isGrounded == false)
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
+        }
+        else if (CheckGround.isGrounded == true)
+        {
+            animator.SetBool("Jump", false);
+            if (rb.velocity.x == 0)
+            {
+                animator.SetBool("Run", false);
+            }
+            else
+            {
+                animator.SetBool("Run", true);
+            }
+        }
+    }
+
+    void isRunning()
+    {
+
+
     }
 }
